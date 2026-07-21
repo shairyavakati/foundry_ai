3d8f8596273cf543d363dafe13612fcd9356d2c8
Shairya Vakati <vakatishairya015@gmail.com>
2026-07-21 15:58:56 +0530

    feat(validation): implement required field validation

--- validations/validation_engine.py
+++ validations/validation_engine.py
@@ -2,7 +2,9 @@ def validate(data, rules):
     errors = {}
     for field_name, field_rules in rules.items():
         field_value = data.get(field_name)
-        if "empty_error" in field_rules:
-            if not field_value:
-                errors[field_name] = field_rules["empty_error"]
+        if field_rules.get("required"):
+            if field_value is None or field_value == "":
+                errors[field_name] = field_rules.get("empty_error", "Field is required")
                 continue
+    return errors
+        
