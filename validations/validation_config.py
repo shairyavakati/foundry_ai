c7a0dc4ee7a31b5461c510248279c8b71c4fbd5d
Shairya Vakati <vakatishairya015@gmail.com>
2026-07-21 15:40:34 +0530

    feat: design reusable validation configuration

--- validations/validation_config.py
+++ validations/validation_config.py
@@ -1,8 +1,34 @@
-USER_VALIDATION_RULES = {
-    "name": {
-        "min_length": 3,
-        "max_length": 100,
-        "empty_error": "Name cannot be empty",
-        "length_error": "Invalid name length"
+VALIDATION_CONFIG = {
+
+    "user": {
+
+        "name": {
+            "required": True,
+            "type": str,
+            "min_length": 3,
+            "max_length": 100
+        },
+
+        "age": {
+            "required": True,
+            "type": int,
+            "min_value": 18,
+            "max_value": 120
+        },
+
+        "email": {
+            "required": True,
+            "type": str,
+            "regex": r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        },
+
+        "phone": {
+            "required": True,
+            "type": str,
+            "length": 10,
+            "regex": r"^\d{10}$"
+        }
+
     }
+
 }
