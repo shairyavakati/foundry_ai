e996871f5deac7e553624847b4f5475b647dea8c
Shairya Vakati <vakatishairya015@gmail.com>
2026-07-23 21:59:14 +0530

    refactor: improve validation engine and initialize repository layer

--- validations/validation_engine.py
+++ validations/validation_engine.py
@@ -1,10 +1,20 @@
+from validations.validation_config import TYPE_MAPPING
+
+
 def validate(data, rules):
     errors = {}
     for field_name, field_rules in rules.items():
         field_value = data.get(field_name)
-        if field_rules.get("required"):
-            if field_value is None or field_value == "":
-                errors[field_name] = field_rules.get("empty_error", "Field is required")
-                continue
-    return errors
+        expected_type = TYPE_MAPPING.get(field_rules.get("type"))
+        
+        if field_rules.get("required") and field_value is None:
+            errors[field_name] = field_rules.get("required_error", "Field is required")
+            continue
+
+        if not isinstance(field_value, expected_type):
+            errors[field_name] = field_rules.get("type_error", f"Field must be of type {expected_type.__name__}")
+            continue
+
+       
+        
         
