fd75c55669a93009d4613ad7fb06e012990097c3
Shairya Vakati <vakatishairya015@gmail.com>
2026-07-28 21:49:41 +0530

    feat(service): add user registration workflow with duplicate email validation

--- services/user_services.py
+++ services/user_services.py
@@ -1,7 +1,16 @@
+from exceptions import EmailAlreadyExistsException
+from repositories import UserRepository
+from models import User
 
-def create_user(user_data):
-    
-    print("Request received in Service Layer.")
-
-    print(user_data)
-
+class UserService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+    def register_user(self, user_data: dict) -> User:
+        # Check if the user already exists
+        existing_user = self.user_repository.find_by_email(user_data["email"])
+        if existing_user:
+            raise EmailAlreadyExistsException(
+            "User with this email already exists"
+             )
+        # Create the new user
+        return self.user_repository.create(user_data)
